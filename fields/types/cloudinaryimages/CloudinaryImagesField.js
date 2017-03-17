f237d6394c5d54899df8d57e91cc2f77321b3e96
merunga <merunga@gmail.com>
2017-03-17 09:37:42 -0500

    error with images in nested list

--- fields/types/cloudinaryimages/CloudinaryImagesField.js
+++ fields/types/cloudinaryimages/CloudinaryImagesField.js
@@ -204,7 +204,10 @@ module.exports = Field.create({
 					type="hidden"
 				/>
 			);
-		} else if (this.getCount('isDeleted') === this.props.value.length) {
+		} else if (
+			this.props.value &&
+			this.getCount('isDeleted') === this.props.value.length
+		) {
 			return (
 				<input
 					name={this.getInputName(this.props.path)}
